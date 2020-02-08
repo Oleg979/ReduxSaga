35ce53722eb3cf4b86033340dfff80dbfe7ae05d
Oleg <olegsolovev506@gmail.com>
2020-02-08 10:44:40 +0400

    Initial commit

---
+++ sagas.js
@@ -0,0 +1,16 @@
+import { delay } from 'redux-saga'
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+function* incrementAsync() {
+    yield delay(500)
+    yield put({ type: 'INCREMENT' })
+}
+
+function* watchIncrementAsync() {
+    yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+export default function* rootSaga() {
+    yield all([
+        watchIncrementAsync()
+    ])
+}
